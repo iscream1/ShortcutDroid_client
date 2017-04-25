bfb5b33856e0efd6deaacf052bfb3f5beb048961
iscream1 <iscream.94@gmail.com>
2017-04-26 01:20:56 +0200

    permanent socket

--- java/com/iscream/shortcutdroid/MainActivity.java
+++ java/com/iscream/shortcutdroid/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Button> buttons;
     LinearLayout buttonLL;
     Socket socket;
+    OutputStream out;
+    PrintWriter output;
     Button connectBtn;
 
     @Override
@@ -66,25 +68,25 @@ public class MainActivity extends AppCompatActivity {
     public void init()
     {
         Log.d("DEBUG", "init");
-        try{
+        /*try{
             socket.close();
         }
         catch (Exception e)
         {
             //e.printStackTrace();
             Log.d("socket", "can't close socket, probably not initialized yet");
-        }
-        buttons=new ArrayList<>();
+        }*/
+        if(buttons==null) buttons=new ArrayList<>();
         final String ipAddress=ipET.getText().toString();
         new AsyncTask<Void, Void, Void>(){
             String line = null;
             @Override
             protected Void doInBackground(Void... voids) {
                 try {
-                    socket = new Socket(ipAddress, 115);
+                    if(socket==null) socket = new Socket(ipAddress, 115);
 
-                    OutputStream out = socket.getOutputStream();
-                    PrintWriter output = new PrintWriter(out);
+                    out = socket.getOutputStream();
+                    output = new PrintWriter(out);
 
                     Log.d("LOG", "Sending setup request to PC");
                     output.print("setup");
@@ -143,5 +145,7 @@ public class MainActivity extends AppCompatActivity {
         {
             buttonLL.addView(btn);
         }
+
+        init();
     }
 }
